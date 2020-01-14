83f30515344b42d9bc38475d92eb222287221819
Nicole Lucien <nicolemlucien@gmail.com>
2020-01-14 19:39:15 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,4 +52,9 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
   
+  def self.find_by_name 
+    
+  
+  end
+  
 end
