cd457ab9ca56ae045cb0e3f2f1484d44467ae0bb
Nicole Lucien <nicolemlucien@gmail.com>
2020-01-14 20:10:16 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -61,9 +61,10 @@ class InteractiveRecord
   def self.find_by(attribute)
     
     attribute.each do |property, value|
-      # binding.pry
+     
     DB[:conn].execute("SELECT * FROM #{table_name} 
     WHERE #{property} = #{value}")
+     binding.pry
     end
   
   end 
